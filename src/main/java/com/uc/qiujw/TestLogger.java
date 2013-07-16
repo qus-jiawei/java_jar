4576278ab68d1b213ca0c0eb401e1d87d98523cd
qus-jiawei <qus.jiawei@gmail.com>
2013-07-16 15:08:35 +0800

    add logger

--- src/main/java/com/uc/qiujw/TestLogger.java
+++ src/main/java/com/uc/qiujw/TestLogger.java
@@ -1,32 +1,33 @@
 package com.uc.qiujw;
 
-import org.apache.hadoop.hdfs.DFSClient;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Logger;
+import java.util.Enumeration;
 
+import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.log4j.Logger;
 
 public class TestLogger implements Run {
 
 	public int run(String[] args) {
-		try{
-//			org.apache.hadoop.hdfs.DFSClient
-//			FileSystem fs = FileSystem.get(new Configuration());
+		try {
+			// org.apache.hadoop.hdfs.DFSClient
+			// FileSystem fs = FileSystem.get(new Configuration());
 			DFSClient.LOG.warn("warn", new Exception("warn exception"));
 			DFSClient.LOG.info("info", new Exception("info exception"));
 			DFSClient.LOG.info("error", new Exception("error exception"));
-			
-			if( DFSClient.LOG instanceof org.apache.log4j.Logger ){
-				Logger log = (  org.apache.log4j.Logger )  DFSClient.LOG;
-				while( log.getAllAppenders().hasMoreElements() ){
-					Object o = log.getAllAppenders().nextElement();
-					System.out.println( o.getClass().getName() );
-				}
+
+			System.out.println(DFSClient.LOG.getClass().getName());
+			Log4JLogger log = (org.apache.commons.logging.impl.Log4JLogger) DFSClient.LOG;
+			Logger native_logger = log.getLogger();
+			System.out.println(native_logger.getClass().getName());
+			Enumeration apps = native_logger.getAllAppenders();
+			while (apps.hasMoreElements()) {
+				Object app = apps.nextElement();
+				System.out.println(app.getClass().getName());
 			}
-			System.out.println( DFSClient.LOG.getClass().getName() ) ;
 			
-		}
-		catch(Exception e){
-			
+		} catch (Exception e) {
+
 		}
 		return 0;
 	}
@@ -34,5 +35,5 @@ public class TestLogger implements Run {
 	public String getKey() {
 		return "testlogger";
 	}
-	
+
 }
