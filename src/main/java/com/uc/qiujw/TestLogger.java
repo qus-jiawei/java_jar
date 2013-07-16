145c930269acda5813993fb85f89cf45a7eafad9
qus-jiawei <qus.jiawei@gmail.com>
2013-07-16 16:07:28 +0800

    add logger

--- src/main/java/com/uc/qiujw/TestLogger.java
+++ src/main/java/com/uc/qiujw/TestLogger.java
@@ -2,11 +2,14 @@ package com.uc.qiujw;
 
 import java.util.Enumeration;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.impl.Log4JLogger;
 import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.log4j.Logger;
 
 public class TestLogger implements Run {
+	public static final Log MYLOG = LogFactory.getLog(TestLogger.class);
 
 	public int run(String[] args) {
 		try {
@@ -25,6 +28,7 @@ public class TestLogger implements Run {
 				Object app = apps.nextElement();
 				System.out.println(app.getClass().getName());
 			}
+			MYLOG.info("haha");
 			Thread.sleep(1000000);
 		} catch (Exception e) {
 
